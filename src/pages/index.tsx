2a2cd057212297913fa3306da5d9c3d1565cb1b7
masyumaro <s1f102300122@iniad.org>
2023-05-02 15:20:39 +0900

    othello

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,120 @@
+import { useState } from 'react';
 import styles from './index.module.css';
+import { dir } from 'console';
 
 const Home = () => {
+  const [turnColor, setTurnColor] = useState(1);
+
+  //prettier-ignore
+  const [board, setBoard] = useState([
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,1,2,0,0,0],
+    [0,0,0,2,1,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+        
+  ]);
+  const dir = []
+
+  const clickCell = (x: number, y: number) => {
+    console.log(x, y);
+    const newBoard = JSON.parse(JSON.stringify(board));
+
+    if (board[y][x] === 0) {
+      for(dir)
+      let a = false;
+      for (let distans = 1; distans < 8; distans += 1) {
+        if (board[y + 1 * distans] === undefined) {
+          break;
+        } else if (board[y + 1 * distans][x + 0 * distans] === 0) {
+          break;
+        } else if (board[y + 1 * distans][x + 0 * distans] === 3 - turnColor) {
+          a = true;
+        } else if (board[y + 1 * distans][x + 0 * distans] === turnColor) {
+          for (let i = distans; i >= 0; i -= 1) {
+            if (a) {
+              newBoard[y + 1 * i][x + 0 * i] = turnColor;
+            }
+          }
+
+          setTurnColor(3 - turnColor);
+          break;
+        }
+      }
+      // else if (
+      //   board[y + 1][x - 1] !== undefined &&
+      //   board[y + 1][x - 1] !== 0 &&
+      //   board[y + 1][x - 1] !== turnColor
+      // ) {
+      //   newBoard[y][x] = turnColor;
+      //   setTurnColor(3 - turnColor);
+      // } else if (
+      //   board[x - 1] !== undefined &&
+      //   board[y][x - 1] !== 0 &&
+      //   board[y][x - 1] !== turnColor
+      // ) {
+      //   newBoard[y][x] = turnColor;
+      //   setTurnColor(3 - turnColor);
+      // } else if (
+      //   board[y - 1][x - 1] !== undefined &&
+      //   board[y - 1][x - 1] !== 0 &&
+      //   board[y - 1][x - 1] !== turnColor
+      // ) {
+      //   newBoard[y][x] = turnColor;
+      //   setTurnColor(3 - turnColor);
+      // } else if (
+      //   board[y - 1] !== undefined &&
+      //   board[y - 1][x] !== 0 &&
+      //   board[y - 1][x] !== turnColor
+      // ) {
+      //   newBoard[y][x] = turnColor;
+      //   setTurnColor(3 - turnColor);
+      // } else if (
+      //   board[y - 1][x + 1] !== undefined &&
+      //   board[y + 1][x + 1] !== 0 &&
+      //   board[y + 1][x + 1] !== turnColor
+      // ) {
+      //   newBoard[y][x] = turnColor;
+      //   setTurnColor(3 - turnColor);
+      // } else if (
+      //   board[y][x + 1] !== undefined &&
+      //   board[y][x + 1] !== 0 &&
+      //   board[y][x + 1] !== turnColor
+      // ) {
+      //   newBoard[y][x] = turnColor;
+      //   setTurnColor(3 - turnColor);
+      // } else if (
+      //   board[y + 1][x + 1] !== undefined &&
+      //   board[y + 1][x + 1] !== 0 &&
+      //   board[y + 1][x + 1] !== turnColor
+      // ) {
+      //   newBoard[y][x] = turnColor;
+      //   setTurnColor(3 - turnColor);
+      // }
+    }
+
+    setBoard(newBoard);
+  };
+
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
-
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div className={styles.board}>
+        {board.map((row, y) =>
+          row.map((color, x) => (
+            <div className={styles.cell} onClick={() => clickCell(x, y)} key={`${x}-${y}`}>
+              {color !== 0 && (
+                <div
+                  className={styles.stone}
+                  style={{ background: color === 1 ? '#000' : '#fff' }}
+                />
+              )}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
