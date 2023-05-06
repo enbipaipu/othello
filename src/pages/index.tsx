7f2a7b23e5066ae9f1139b80b72ad76a51be7211
masyumaro <s1f102300122@iniad.org>
2023-05-06 22:36:06 +0900

    リセットボタン追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,16 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const [initialBoard, setInitialBoard] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 3, 0, 0, 0, 0],
+    [0, 0, 3, 2, 1, 0, 0, 0],
+    [0, 0, 0, 1, 2, 3, 0, 0],
+    [0, 0, 0, 0, 3, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
   const direction = [
     [-1, 0],
     [-1, 1],
@@ -25,25 +35,14 @@ const Home = () => {
     [-1, -1],
   ];
 
+  const resetBoard = () => {
+    setBoard(initialBoard);
+    setTurnColor(1);
+  };
+
   const clickCell = (x: number, y: number) => {
     console.log(x, y);
     const newBoard = JSON.parse(JSON.stringify(board));
-
-    const cellCount = function () {
-      const total = 81;
-      let brake = 0;
-      let blank = 0;
-      for (let len = 0; len < 8; len += 1) {
-        for (let wid = 0; wid < 8; wid += 1) {
-          if (board[len][wid] === 0) {
-            blank += 1;
-          } else if (board[len][wid] === 1) {
-            brake += 1;
-          }
-        }
-      }
-      const white = total - brake - blank;
-    };
 
     function checkBoard(color: number) {
       for (let i = 0; i < 8; i += 1) {
@@ -110,7 +109,6 @@ const Home = () => {
         setBoard(newBoard);
         checkBoard(turnColor);
         setTurnColor(3 - turnColor);
-        cellCount();
       }
     }
     if (newBoard.some((row: number[]) => row.includes(3)) === false) {
@@ -119,29 +117,90 @@ const Home = () => {
       alert(
         `石を置ける場所がないため${3 - turnColor === 1 ? '黒' : '白'}のターンがスキップされます`
       );
-      cellCount();
     }
   };
 
+  let blackStones = 0;
+  let whiteStones = 0;
+
+  // `board`の要素をループして、`color`の値に応じて`blackStones`と`whiteStones`をカウントします
+  board.forEach((row) => {
+    row.forEach((color) => {
+      if (color === 1) {
+        blackStones++;
+      } else if (color === 2) {
+        whiteStones++;
+      }
+    });
+  });
+
   return (
+    // <div className={styles.container}>
+    //   {/* リセットボタンのJSXを追加 */}
+    //   <button className={styles.button} onClick={resetBoard}>
+    //     リセット
+    //   </button>
+    //   &emsp;
+    //   <div className={styles.board}>
+    //     {board.map((row, y) =>
+    //       row.map((color, x) => (
+    //         <div
+    //           className={styles.cell + (color === 3 ? ` ${styles.orangeBorder}` : '')}
+    //           onClick={() => clickCell(x, y)}
+    //           key={`${x}-${y}`}
+    //         >
+    //           {color !== 0 && color !== 3 && (
+    //             <div
+    //               className={styles.stone}
+    //               style={{ background: color === 1 ? '#000' : '#fff' }}
+    //             />
+    //           )}
+    //         </div>
+    //       ))
+    //     )}
+    //   </div>
+    //   <div className={styles.turn}>
+    //     <h1>{turnColor === 1 ? '黒' : '白'}の番です</h1>
+    //   </div>
+    //   <div className={`${styles.space} ${styles.score}`}>
+    //     <h1>
+    //       ＜得点＞&emsp;黒: {blackStones} &emsp;白: {whiteStones}
+    //     </h1>
+    //   </div>
+    // </div>
     <div className={styles.container}>
       <div className={styles.board}>
         {board.map((row, y) =>
           row.map((color, x) => (
-            <div className={styles.cell} onClick={() => clickCell(x, y)} key={`${x}-${y}`}>
-              {color !== 0 && (
+            <div
+              className={styles.cell + (color === 3 ? ` ${styles.orangeBorder}` : '')}
+              onClick={() => clickCell(x, y)}
+              key={`${x}-${y}`}
+            >
+              {color !== 0 && color !== 3 && (
                 <div
                   className={styles.stone}
-                  style={{ background: color === 1 ? '#000' : color === 2 ? '#fff' : '#ff8400' }}
+                  style={{ background: color === 1 ? '#000' : '#fff' }}
                 />
               )}
             </div>
           ))
         )}
       </div>
-      <h1>{turnColor === 1 ? '黒' : '白'}の番です</h1>
-
-      <h1>黒: 白:</h1>
+      <div className={styles.sidePanel}>
+        <div className={styles.turn}>
+          <h1>{turnColor === 1 ? '黒' : '白'}の番です</h1>
+        </div>
+        <div className={styles.score}>
+          <h1>
+            ＜得点＞&emsp;黒: {blackStones} &emsp;白: {whiteStones}
+          </h1>
+        </div>
+        {/* リセットボタン */}
+        <button className={styles.button} onClick={resetBoard}>
+          リセット
+        </button>
+      </div>
     </div>
   );
 };
